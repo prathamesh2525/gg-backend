35d9ec113dbf055b404046aafa53f138b0bbb8d1
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2024-03-26 16:00:26 +0530

    updated the secrets and .env file

--- utils/index.js
+++ utils/index.js
@@ -4,7 +4,7 @@ const axios = require("axios")
 async function fetchWeather(city, date) {
   try {
     const response = await axios.get(
-      `https://gg-backend-assignment.azurewebsites.net/api/Weather?code=KfQnTWHJbg1giyB_Q9Ih3Xu3L9QOBDTuU5zwqVikZepCAzFut3rqsg==&city=${city}&date=${date}`
+      `${process.env.WEATHER_API_URL}&city=${city}&date=${date}`
     )
     return response.data.weather
   } catch (error) {
@@ -16,7 +16,7 @@ async function fetchWeather(city, date) {
 async function calculateDistance(lat1, lon1, lat2, lon2) {
   try {
     const response = await axios.get(
-      `https://gg-backend-assignment.azurewebsites.net/api/Distance?code=IAKvV2EvJa6Z6dEIUqqd7yGAu7IZ8gaH-a0QO6btjRc1AzFu8Y3IcQ==&latitude1=${lat1}&longitude1=${lon1}&latitude2=${lat2}&longitude2=${lon2}`
+      `${process.env.DISTANCE_API_URL}&latitude1=${lat1}&longitude1=${lon1}&latitude2=${lat2}&longitude2=${lon2}`
     )
     return response.data.distance
   } catch (error) {
